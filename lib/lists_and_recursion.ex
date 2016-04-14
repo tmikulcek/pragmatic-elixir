1f4e82caecfd49d9d4074ead167508c1df248a77
tom <tomislav.mikulcek@virgo.hu>
2016-04-14 09:41:47 +0200

    simplify all?

--- lib/lists_and_recursion.ex
+++ lib/lists_and_recursion.ex
@@ -1,13 +1,9 @@
 defmodule Lists_and_recursion do
   @moduledoc false
 
-  def all?(list, is_true?) do
-    _all?(list, is_true?)
-  end
+  def all?([], _fun), do: true
 
-  defp _all?([], _fun), do: true
-
-  defp _all?([head|tail], is_true?) do
-    if is_true?.(head), do: _all?(tail, is_true?), else: false
+  def all?([head|tail], is_true?) do
+    if is_true?.(head), do: all?(tail, is_true?), else: false
   end
 end
