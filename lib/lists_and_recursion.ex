a7a0a25faa317d63aa721e1a877c74576bec3d87
tomislav.mikulcek <tomislav.mikulcek@virgo.hu>
2016-04-14 21:29:21 +0200

    simplify split

--- lib/lists_and_recursion.ex
+++ lib/lists_and_recursion.ex
@@ -25,21 +25,19 @@ defmodule Lists_and_recursion do
         _split(Enum.reverse(list), 0 - count, {[], []}, true)
     end
 
+    def split([], _), do: {[], []}
     def split(list, count) do
         _split(list, count, {[], []}, false)
     end
-
-    def _split([], _count, {l1, l2}, reverse?) when reverse?, do: {l2, l1}
-    def _split([], _count, acc, _), do: acc
 
     def _split(list = [head | tail], count, {l1, l2}, reverse?) do
         if count > 0 do
             _split(tail, count - 1, {l1 ++ [head], l2}, reverse?)
         else
             if reverse? do
-                {Enum.reverse(l2 ++ list), l1}
+                {Enum.reverse(list), l1}
             else
-                {l1, l2 ++ list}
+                {l1, list}
             end
         end
   end
