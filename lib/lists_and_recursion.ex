aaf5a93bca3a6c058d5cee2faf739308f4c13a97
tomislav.mikulcek <tomislav.mikulcek@virgo.hu>
2016-04-14 19:39:30 +0200

    implement filter

--- lib/lists_and_recursion.ex
+++ lib/lists_and_recursion.ex
@@ -2,13 +2,19 @@ defmodule Lists_and_recursion do
   @moduledoc false
 
   def all?([], _fun), do: true
-  def all?([head|tail], is_true?) do
+  def all?([head | tail], is_true?) do
     if is_true?.(head), do: all?(tail, is_true?), else: false
   end
 
   def each([], _fun), do: :ok
-  def each([head|tail], fun) do
+  def each([head | tail], fun) do
     fun.(head)
     each(tail, fun)
+  end
+
+  def filter([], _fun), do: []
+  def filter([head | tail], fun) do
+    if fun.(head), do: [head|filter(tail, fun)],
+    else: filter(tail, fun)
   end
 end
--- test/lists_and_recursion_test.exs
+++ test/lists_and_recursion_test.exs
@@ -3,16 +3,22 @@ defmodule Lists_and_recursionTest do
   import ExUnit.CaptureIO
   doctest PragmaticElixir
 
+  def is_even(num), do: rem(num, 2) == 0
+
   test "all? all are even" do
-    assert Enum.all?([2, 4, 6, 8], &(rem(&1, 2) == 0)) == Lists_and_recursion.all?([2, 4, 6, 8], &(rem(&1, 2) == 0))
+    assert Enum.all?([2, 4, 6, 8], &is_even/1) == Lists_and_recursion.all?([2, 4, 6, 8], &is_even/1)
   end
 
   test "all? not all are even" do
-    assert Enum.all?([1, 2, 3], &(rem(&1, 2) == 0)) == Lists_and_recursion.all?([1,2,3], &(rem(&1, 2) == 0))
+    assert Enum.all?([1, 2, 3], &is_even/1) == Lists_and_recursion.all?([1,2,3], &is_even/1)
   end
 
   test "each" do
     testable_side_effects = fn -> assert :ok == Lists_and_recursion.each([1, 2, 3], &IO.puts/1) end
     assert capture_io(testable_side_effects) == "1\n2\n3\n"
+  end
+
+  test "filter" do
+    assert Enum.filter([1, 2, 3, 4], &is_even/1) == Lists_and_recursion.filter([1, 2, 3, 4], &is_even/1)
   end
 end
