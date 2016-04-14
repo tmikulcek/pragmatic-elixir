40e3a38398a4cb006eb98c0b6773d0280ccefa1e
tomislav.mikulcek <tomislav.mikulcek@virgo.hu>
2016-04-14 18:49:38 +0200

    update test to equal original Enum functions

--- test/lists_and_recursion_test.exs
+++ test/lists_and_recursion_test.exs
@@ -3,10 +3,10 @@ defmodule Lists_and_recursionTest do
   doctest PragmaticElixir
 
   test "all are even" do
-    assert true == Lists_and_recursion.all?([2, 4, 6, 8], &(rem(&1, 2) == 0))
+    assert Enum.all?([2, 4, 6, 8], &(rem(&1, 2) == 0)) == Lists_and_recursion.all?([2, 4, 6, 8], &(rem(&1, 2) == 0))
   end
 
   test "not all are even" do
-    assert false == Lists_and_recursion.all?([1,2,3], &(rem(&1, 2) == 0))
+    assert Enum.all?([1, 2, 3], &(rem(&1, 2) == 0)) == Lists_and_recursion.all?([1,2,3], &(rem(&1, 2) == 0))
   end
 end
