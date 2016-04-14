eb461321b3118f3cf2e4bab6052a4b06422b8a7c
tomislav.mikulcek <tomislav.mikulcek@virgo.hu>
2016-04-14 19:28:27 +0200

    implement each

--- lib/lists_and_recursion.ex
+++ lib/lists_and_recursion.ex
@@ -2,8 +2,13 @@ defmodule Lists_and_recursion do
   @moduledoc false
 
   def all?([], _fun), do: true
-
   def all?([head|tail], is_true?) do
     if is_true?.(head), do: all?(tail, is_true?), else: false
+  end
+
+  def each([], _fun), do: :ok
+  def each([head|tail], fun) do
+    fun.(head)
+    each(tail, fun)
   end
 end
--- test/lists_and_recursion_test.exs
+++ test/lists_and_recursion_test.exs
@@ -1,12 +1,18 @@
 defmodule Lists_and_recursionTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest PragmaticElixir
 
-  test "all are even" do
+  test "all? all are even" do
     assert Enum.all?([2, 4, 6, 8], &(rem(&1, 2) == 0)) == Lists_and_recursion.all?([2, 4, 6, 8], &(rem(&1, 2) == 0))
   end
 
-  test "not all are even" do
+  test "all? not all are even" do
     assert Enum.all?([1, 2, 3], &(rem(&1, 2) == 0)) == Lists_and_recursion.all?([1,2,3], &(rem(&1, 2) == 0))
+  end
+
+  test "each" do
+    testable_side_effects = fn -> assert :ok == Lists_and_recursion.each([1, 2, 3], &IO.puts/1) end
+    assert capture_io(testable_side_effects) == "1\n2\n3\n"
   end
 end
