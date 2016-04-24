bae8c42e93f46e93765e732bce73ce2af9ae3c27
Tomislav Mikulček <tmikulcek@gmail.com>
2016-04-24 15:38:48 +0200

    list comprehension

--- lib/lists_and_recursion.ex
+++ lib/lists_and_recursion.ex
@@ -52,4 +52,30 @@ defmodule Lists_and_recursion do
         diff = rangeEnd - start + 1
         Enum.drop(list, start) |> take(diff)
     end
+
+    def calculate_total() do
+        tax_rates = [NC: 0.075, TX: 0.08]
+        orders = [
+            [id: 123, ship_to: :NC, net_amount: 100.00],
+            [id: 124, ship_to: :OK, net_amount: 35.50],
+            [id: 125, ship_to: :TX, net_amount: 24.00],
+            [id: 126, ship_to: :TX, net_amount: 44.80],
+            [id: 127, ship_to: :NC, net_amount: 25.00],
+            [id: 128, ship_to: :MA, net_amount: 10.00],
+            [id: 129, ship_to: :CA, net_amount: 102.00],
+            [id: 120, ship_to: :NC, net_amount: 50.00],
+        ]
+
+        total(tax_rates, orders)
+    end
+
+    def total(tax_rates, orders) do
+        for order <- orders do
+            case Keyword.fetch(tax_rates, order[:ship_to]) do
+                {:ok, tax_rate} -> [{:total_amount, order[:net_amount] + order[:net_amount] * tax_rate}| order]
+                _ -> [{:total_amount, order[:net_amount]}| order]
+            end
+        end
+
+    end
 end
